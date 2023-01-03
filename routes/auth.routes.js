e03baed8da7228ef2906a2689484a5e7b6a2e6f0
Giulia Colombo <giulia.colombo.127@gmail.com>
2023-01-03 17:17:57 +0100

    login routes and views done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -23,7 +23,35 @@ router.post('/signup', (req, res) => {
         .catch(err => console.log(err))
 })
 
-//login route
+//login GET route
+router.get('/login', (req, res) => res.render('auth/login'));
+
+//login POST route
+router.post('/login', (req, res, next) => {
+    const { email, password } = req.body;
+    //const email = req.body.email;
+    //const password = req.body.password;
+
+    if (email === '' || password === '') {
+        res.render('auth/login', {
+            errorMessage: "Please enter both email and password to login."
+        });
+        return;
+    }
+
+    User.findOne({email})
+    .then(user => {
+        if (!user) {
+            res.render('auth/login', { errorMessage: "Email is not registered. Try with another email."});
+            return;
+        } else if (bcrypt.compareSync(password, user.passwordHash)) {
+            res.render('auth/profile', { user });
+        } else {
+            res.render('auth/login', {errorMessage: "Incorrect password"})
+        }
+    })
+    .catch(err => console.log(err))
+})
 
 //profile route
 router.get('/profile/:username', (req, res) => {
